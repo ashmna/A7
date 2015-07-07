28899d526c127d5e8ee7ebc69810f3b666e7621a
ashmna <mnatsakanyan.ashot@gmail.com>
2015-07-07 17:08:20 +0400

    - Added many function in ReflectionUtilsInterface

--- src/A7/ReflectionUtilsInterface.php
+++ src/A7/ReflectionUtilsInterface.php
@@ -4,5 +4,31 @@ namespace A7;
 
 
 interface ReflectionUtilsInterface {
-    function get
+    /**
+     * @param $className
+     * @return \ReflectionClass
+     */
+    function getClassReflection($className);
+    /**
+     * @param $className
+     * @return \ReflectionProperty[]
+     */
+    function getPropertiesReflection($className);
+    /**
+     * @param $className
+     * @return \ReflectionMethod[]
+     */
+    function getMethodsReflection($className);
+    /**
+     * @param $className
+     * @param $methodName
+     * @return \ReflectionMethod
+     */
+    function getMethodReflection($className, $methodName);
+    /**
+     * @param $className
+     * @param $methodName
+     * @return \ReflectionParameter[]
+     */
+    function getParametersReflection($className, $methodName);
 }
