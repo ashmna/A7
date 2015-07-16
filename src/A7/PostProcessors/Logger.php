32d3b2028ae7587a2ef2dc918f6e9d32ab980210
ashmna <mnatsakanyan.ashot@gmail.com>
2015-07-16 11:17:22 +0400

    - Change Logger Post Processor

--- src/A7/PostProcessors/Logger.php
+++ src/A7/PostProcessors/Logger.php
@@ -10,37 +10,40 @@ use A7\Proxy;
 
 class Logger implements PostProcessInterface {
 
-    protected $a7;
-    /** @var AnnotationManagerInterface  */
-    protected $annotationManager;
+    private $a7;
+    private $parameters;
 
-    protected $usedClass;
 
     function postProcessBeforeInitialization($instance, $className) {
-        if(isset($this->usedClass[$className])) return $instance;
-
-        if(!($instance instanceof Proxy)) {
-            $instance = new Proxy($this->a7, $className, $instance);
-        }
-
-        $this->usedClass[$className] = true;
-
-        $instance->a7AddBeforeCall(function($arguments, $className, $methodName, &$result, &$isCallable) {
-            $result = 111111111111;
-            $isCallable = false;
-        });
-
-        $instance->a7AddAfterCall(function($result) {
-
-        });
-
-
-
         return $instance;
     }
 
     function postProcessAfterInitialization($instance, $className) {
+        if(!($instance instanceof Proxy)) {
+            $instance = new Proxy($this->a7, $className, $instance);
+        }
+
+        $instance->a7AddBeforeCall([$this, 'beforeCall']);
+        $instance->a7AddAfterCall( [$this, 'afterCall'] );
+
         return $instance;
+    }
+
+
+    function beforeCall($className, $methodName, $arguments, &$params) {
+        $argumentsString = var_export($arguments, true);
+        $date = date('Y-m-d H:i:s');
+        file_put_contents($this->parameters['path'], "$date: $className->$methodName  arguments: \n$argumentsString\n", FILE_APPEND);
+        $params['startTime'] = microtime(true);
+    }
+
+    function afterCall($className, $methodName, $result, &$params) {
+        $endTime   = microtime(true);
+        $startTime = $params['startTime'];
+        $executeTime = $endTime - $startTime;
+        $date = date('Y-m-d H:i:s');
+        $resultString = var_export($result, true);
+        file_put_contents($this->parameters['path'], "$date: $className->$methodName  executeTime: $executeTime, result:\n$resultString\n", FILE_APPEND);
     }
 
 
