9e819bdff5028e863af9b474edb121b596dfea24
ashmna <mnatsakanyan.ashot@gmail.com>
2016-01-21 02:06:17 +0400

    - Added invokeMethod and getMember for protected methods and members

--- Tests/Resources/AbstractUnitTestCase.php
+++ Tests/Resources/AbstractUnitTestCase.php
@@ -11,4 +11,20 @@ abstract class AbstractUnitTestCase extends \PHPUnit_Framework_TestCase
         $property->setAccessible(true);
         $property->setValue($class, $mock);
     }
+
+    protected function invokeMethod($class, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionObject($class);
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($class, $parameters);
+    }
+
+    protected function getMember($class, $memberName)
+    {
+        $reflection = new \ReflectionObject($class);
+        $member = $reflection->getProperty($memberName);
+        $member->setAccessible(true);
+        return $member->getValue($class);
+    }
 }
